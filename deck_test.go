bb2e27102df30375e4f2cd04ec9ddd3337f5d38b
Victor Matheus Tibirica Batata <victorbatata@itstransdata.com>
2022-07-22 13:23:21 -0300

    Finishing project

--- deck_test.go
+++ deck_test.go
@@ -1,11 +1,37 @@
 package main
 
-import "testing"
+import (
+	"os"
+	"testing"
+)
 
 func TestNewDec(t *testing.T) {
 	d := newDeck()
 
 	if len(d) != 52 {
-		t.Errorf("Expected deck length of 52, but got", len(d))
+		t.Errorf("Expected deck length of 52, but got %v", len(d))
 	}
+
+	if d[0] != "Ace of Spades" {
+		t.Errorf("Expected first card of Ace of Spades, but got %v", d[0])
+	}
+
+	if d[len(d)-1] != "King of Clubs" {
+		t.Errorf("Expected last card of King of Clubs, but got %v", d[len(d)-1])
+	}
+}
+
+func TestSaveToFileAndNewDeckFromFile(t *testing.T) {
+	os.Remove("_decktesting")
+
+	d := newDeck()
+	d.saveToFile("_decktesting")
+
+	loadedDeck := newDeckFromFile("_decktesting")
+
+	if len(loadedDeck) != 52 {
+		t.Errorf("Expected deck length of 52, but got %v", len(loadedDeck))
+	}
+
+	os.Remove("_decktesting")
 }
